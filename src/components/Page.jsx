9780ee07d0aa5d731e716089e0c493c8b933c20c
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-01-04 23:37:11 +0330

    Initial setup of Page component

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,39 @@
-export default function Page() {
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import Header from './Header';
+import Menu from './Menu';
+import Footer from './Footer';
+
+export default function Page({
+  title,
+  children,
+  footer,
+  backPage,
+  nextText,
+  nextPage,
+  nextFunc,
+}) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleCloseMenu = () => setIsMenuOpen(false);
+
   return (
-    <div>Page</div>
-  )
+    <motion.div
+      className='page'
+      initial={{ opacity: 0, transform: 'translateY(-1rem)' }}
+      animate={{ opacity: 1, transform: 'translateY(0)' }}
+    >
+      <Header title={title} setIsMenuOpen={setIsMenuOpen} />
+      <Menu isOpen={isMenuOpen} handleCloseMenu={handleCloseMenu} />
+
+      {children}
+
+      <Footer
+        footer={footer}
+        backPage={backPage}
+        nextText={nextText}
+        nextPage={nextPage}
+        nextFunc={nextFunc}
+      />
+    </motion.div>
+  );
 }
