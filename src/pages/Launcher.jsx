1c56dd5e77d854c77c9861d18f5142f46c794e0d
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-01-04 19:23:02 +0330

    Initial setup Launcher page

--- src/pages/Launcher.jsx
+++ src/pages/Launcher.jsx
@@ -1,3 +1,37 @@
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import useTheme from '../hooks/useTheme';
+
 export default function Launcher() {
-  return <div>launcher</div>;
+  const { theme } = useTheme();
+
+  return (
+    <motion.div
+      className='max-w-3xl h-screen dark:bg-gray-800 relative overflow-x-hidden mx-auto'
+      initial={{ opacity: 0, transform: 'translateY(-1rem)' }}
+      animate={{ opacity: 1, transform: 'translateY(0)' }}
+    >
+      <img
+        className='w-full h-full absolute object-cover'
+        src={theme === 'dark' ? 'logo-light.png' : 'logo-dark.png'}
+        alt='logo'
+      />
+
+      <div className='w-max max-w-60 text-center absolute bottom-36 inset-x-1/2 -translate-x-1/2'>
+        <h1 className='text-gray-800 dark:text-gray-300'>
+          <span className='text-6xl font-black'>Q.G</span>
+          <span className='text-3xl font-bold'> Game</span>
+        </h1>
+        <p className='font-medium text-gray-800 dark:text-gray-400 leading-5 mt-2'>
+          This is a simple Question and Answer Game as we call it
+          <br /> <span className='font-semibold'>( Quiz Game )</span>
+        </p>
+        <Link to='/hint'>
+          <button className='bg-gray-800 dark:bg-gray-700 text-lg font-medium text-white dark:text-gray-300 mt-6 tracking-widest px-12 py-2 rounded shadow-xl'>
+            Start
+          </button>
+        </Link>
+      </div>
+    </motion.div>
+  );
 }
