64f2984cf72698f8cde552ca60646ceb191b4442
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-01-04 23:36:10 +0330

    Update some props

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
 
 export default function Footer(props) {
-  const { isFooter, backPage, nextText, nextPage, nextFunc } = props;
-  isFooter = true;
+  let { footer = true, backPage, nextText, nextPage, nextFunc } = props;
 
   return (
     <>
-      {isFooter && (
-        <div className='bg-cream dark:bg-gray-800 w-full h-40 absolute z-10 bottom-0 inset-x-0 pt-6 px-6 flex items-center justify-between border-t border-gray-700 transition-colors'>
+      {footer && (
+        <div className='bg-cream dark:bg-gray-800 w-full h-40 absolute z-10 bottom-0 inset-x-0 pt-6 px-6 flex justify-between border-t border-gray-700 transition-colors'>
           <Link to={backPage}>
             <button className='bg-gray-700 text-white w-32 h-12 flex items-center justify-center font-semibold rounded'>
               Back
