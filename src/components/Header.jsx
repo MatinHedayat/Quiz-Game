0bbbc262ab1838ccd0f7d3278c51b1b8f79b2a15
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-01-04 23:36:43 +0330

    Initial setup of Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,35 @@
-export default function Header() {
+import { HiMenuAlt2 } from 'react-icons/hi';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export default function Header({ title, setIsMenuOpen }) {
+  const character = useSelector((state) => state.character);
+  const navigate = useNavigate();
+
   return (
-    <div>Header</div>
-  )
+    <div className='bg-gray-700 text-white/80 h-14 px-6 mb-10 flex items-center justify-between rounded'>
+      <button onClick={() => setIsMenuOpen(true)}>
+        <HiMenuAlt2 className=' text-2xl' />
+      </button>
+
+      <div className='flex items-center gap-x-4'>
+        <div className='font-medium flex items-center gap-4'>
+          <span>{title}</span>
+          <span>|</span>
+        </div>
+
+        <button
+          className='w-7 h-7 flex overflow-hidden rounded-full'
+          onClick={() => navigate('/character')}
+        >
+          {character.isLogin ? (
+            <img className='w-full' src={character.avatar} alt='character' />
+          ) : (
+            <FaRegUserCircle className='w-full h-full p-0.5' />
+          )}
+        </button>
+      </div>
+    </div>
+  );
 }
