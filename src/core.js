94884d5b9c3dc28d0a8e5d31e8b70cfa0166cf1e
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-01-04 23:34:42 +0330

    Update reducer of store

--- src/core.js
+++ src/core.js
@@ -1,6 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import matchDataReducer from './features/matchDataSlice';
+import characterReducer from './features/characterSlice';
+import settingReducer from './features/settingSlice';
 
 export const core = configureStore({
-  reducer: { matchData: matchDataReducer },
+  reducer: {
+    matchData: matchDataReducer,
+    character: characterReducer,
+    setting: settingReducer,
+  },
 });
