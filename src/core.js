1fe7ee3ea953596474ec72506673ece561831c64
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2024-01-07 19:53:33 +0330

    Some update on redux store (core)

--- src/core.js
+++ src/core.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import matchDataReducer from './features/matchDataSlice';
+import historyReducer from './features/historySlice';
 import characterReducer from './features/characterSlice';
 import settingReducer from './features/settingSlice';
 
 export const core = configureStore({
   reducer: {
     matchData: matchDataReducer,
+    history: historyReducer,
     character: characterReducer,
     setting: settingReducer,
   },
